f47eaf285d0aa790d6ccbfeaefa5eaed45b6fc06
Samuel Pletain <samuel.pletain@gmail.com>
2023-04-19 10:27:34 -0600

    Fixing unhandled promise rejections

--- controllers/contactsController.js
+++ controllers/contactsController.js
@@ -2,29 +2,36 @@ const db = require('../db/connection');
 const ObjectId = require('mongodb').ObjectId;
 
 const getAllContacts = (req, res, next) => {
-  db.then(client => {
-    let collection = client.db("CSE341").collection('contacts');
-    collection.find({}).toArray().then((data) => {
-      res.send(data).status(200);
-    });
-  });  
+  try {
+    db.then(client => {
+      let collection = client.db("CSE341").collection('contacts');
+      collection.find({}).toArray().then((data) => {
+        res.send(data).status(200);
+      });
+    });  
+  } catch (err) {
+    next(err);
+  }
 }
 
 const getContactById = (req, res, next) => {
-  let id = ""
   try {
-    id = new ObjectId(req.params.contactId);
+    let id = ""
+    try {
+      id = new ObjectId(req.params.contactId);
+    } catch (err) {
+      res.status(400).send("Please provide a valid contact id.");
+      return;
+    }
+    db.then(client => {
+      let collection = client.db("CSE341").collection('contacts');
+      collection.find({_id: id}).toArray().then((data) => {
+        res.send(data).status(200);      
+      });
+    });  
   } catch (err) {
-    res.status(400).send("Please provide a valid contact id.");
-    return;
-  }
-  db.then(client => {
-    let collection = client.db("CSE341").collection('contacts');
-    collection.find({_id: id}).toArray().then((data) => {
-      res.send(data).status(200);      
-    });
-  });  
-  
+    next(err);
+  }  
 }
 
 module.exports = { getAllContacts, getContactById };
