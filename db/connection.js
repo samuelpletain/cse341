005c41329172eb9cafcb59e5ddbffa699462e8ed
Samuel Pletain <samuel.pletain@gmail.com>
2023-04-19 10:47:17 -0600

    Fixing mongodb connection error

--- db/connection.js
+++ db/connection.js
@@ -2,8 +2,10 @@ const connectionString = process.env.ATLAS_URI || "";
 
 const MongoClient = require('mongodb').MongoClient
 
+let db;
+
 try {
-  const db = MongoClient.connect(connectionString);
+  db = MongoClient.connect(connectionString);
 } catch (err) {
   console.log(err.message);
 }
