26fb4aa66f4f5344c12b3e89032e87fffbddb479
Samuel Pletain <samuel.pletain@gmail.com>
2023-05-09 18:33:01 -0600

    Fixed headers

--- server.js
+++ server.js
@@ -15,15 +15,18 @@ app.use(
   })
 );
 
-app.use(express.json()).use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  res.setHeader("Content-Type", "application/json");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  next();
-}).use("/", require("./routes"));
-
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+app
+  .use(express.json())
+  .use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    res.setHeader("Content-Type", "application/json");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    next();
+  })
+  .use("/", require("./routes"));
 
 db.initDb((err) => {
   if (err) {
