b14c651200301e5161938e6a9a539f612248ee87
Samuel Pletain <samuel.pletain@gmail.com>
2023-05-09 18:21:08 -0600

    Fixed headers setting

--- server.js
+++ server.js
@@ -15,14 +15,13 @@ app.use(
   })
 );
 
-app.use(express.json()).use((req, res) => {
+app.use(express.json()).use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   res.setHeader("Content-Type", "application/json");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-});
-
-app.use("/", require("./routes"));
+  next();
+}).use("/", require("./routes"));
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
